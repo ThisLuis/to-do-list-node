91647fd0837630def7443a243638025a8e02671b
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-03-23 22:17:08 -0600

    Transformamos nuestro objeto de tareas a un arreglo

--- models/tasks.js
+++ models/tasks.js
@@ -4,6 +4,17 @@ class Tasks {
 
     _list = {};
 
+    get listArr() {
+        const list = [];
+        // ? Con Object.keys extraemos todas las llaves que tenga el arreglo _list
+        Object.keys(this._list).forEach( key => {
+            const task = this._list[key];
+            list.push( tarea );
+        });
+
+        return list;
+    }
+
     constructor() {
         this._list = {};
     }
